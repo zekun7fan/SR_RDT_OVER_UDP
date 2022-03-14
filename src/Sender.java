9cb4d6c75073a41a87e6adea903836ee181dec63
Zekun Fan <z62fan@uwaterloo.ca>
2022-03-14 16:39:34 -0400

    m10

--- src/Sender.java
+++ src/Sender.java
@@ -157,6 +157,7 @@ public class Sender {
         Sender sender = new Sender(args[0], args[1], args[2], args[3], args[4]);
         sender.sendNewPacket();
         sender.listen();
+        System.out.println("main close");
     }
 }
 
